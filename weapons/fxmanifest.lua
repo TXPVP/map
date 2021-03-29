36a6650fbefabb49d429bde346a0be4dc57b72cc
Hamy-os <hamzaalisic0@gmail.com>
2021-03-29 21:08:18 +0300

    feat(Main/Weapons) Added weapons and better hud elements
    these weapons still need optimisations done but are ready for use

---
+++ weapons/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Hamy-os'
+description 'gun pack'
+version '1.0.0'
+
+data_file 'WEAPONINFO_FILE_PATCH' 'data/*/weapons.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/**/weaponanimations.meta'
+data_file 'FIVEM_LOVES_YOU_447B37BE29496FA0' 'data/*/explosion.ymt'
+data_File 'WEAPONCOMPONENTSINFO_FILE' 'data/*/weaponcomponents.meta'
