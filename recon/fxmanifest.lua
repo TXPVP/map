d0222debf58e2b1c1f2dc3222a6c0c34aa47fbb9
Hamy-os <hamzaalisic0@gmail.com>
2021-03-29 19:10:29 +0300

    feat(Main/weapons) Added smoke grenade launcher and updated drone
    Added smoke grenade launcher and updated drone resource to use fxmanifest

---
+++ recon/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Hamy-os'
+description 'Drone'
+version '1.0.0'
+
+files {
+    'vehicles.meta',
+    'handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+
+
