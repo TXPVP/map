3d3b30fb20244dfd25f110699b53641b6e7a7a0b
Hamy-os <hamzaalisic0@gmail.com>
2021-03-30 20:52:47 +0300

    fix(Main/weapons) Fixed datafiles in manifest and added ptfx for smoke launcher

--- weapons/fxmanifest.lua
+++ weapons/fxmanifest.lua
@@ -5,7 +5,17 @@ author 'Hamy-os'
 description 'gun pack'
 version '1.0.0'
 
+files {
+    'data/*/ptfxassetinfo.meta',
+    'data/*/weapons.meta',
+    'data/*/weaponcomponents.meta',
+    'data/*/weapon*.meta',
+    'data/*/weaponanimations.meta',
+    'data/*/explosion.ymt'
+}
+
 data_file 'WEAPONINFO_FILE_PATCH' 'data/*/weapons.meta'
 data_file 'WEAPON_ANIMATIONS_FILE' 'data/**/weaponanimations.meta'
 data_file 'FIVEM_LOVES_YOU_447B37BE29496FA0' 'data/*/explosion.ymt'
 data_File 'WEAPONCOMPONENTSINFO_FILE' 'data/*/weaponcomponents.meta'
+data_file 'PTFXASSETINFO_FILE' 'data/ptfxassetinfo.meta'
